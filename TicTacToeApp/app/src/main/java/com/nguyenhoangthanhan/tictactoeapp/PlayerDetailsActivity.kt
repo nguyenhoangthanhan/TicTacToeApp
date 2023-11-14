f05e375e2032623e8fa6c161c6a62f3a79e0852a
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-11-14 16:58:28 +0700

    Completed Home Screen of TicTacToe App

---
+++ TicTacToeApp/app/src/main/java/com/nguyenhoangthanhan/tictactoeapp/PlayerDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.nguyenhoangthanhan.tictactoeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_details)
+    }
+}
